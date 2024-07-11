898e38c28c9bb1ffaa0341820747633c67e3d15d
v-Aneta.Zelik <aneta.zelik@yahoo.com>
2024-07-11 16:26:39 +0200

    Step counter prep added

---
+++ README.md
@@ -0,0 +1,13 @@
+SensorManager for retrieving steps data from a mobile device.
+Room for local data storage.
+Health Connect for storing and sharing health and fitness data on device.
+
+Request permissions
+ACTIVITY_RECOGNITION permission in manifest
+to obtain location
+
+activity types
+
+ExerciseClient
+
+functions: Pause, resume, and end a workout
---
+++ app/src/main/java/com/example/fit/StepCounter.java
@@ -0,0 +1,58 @@
+package com.example.fit;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.Toast;
+
+public class StepCounter implements SensorEventListener {
+    private boolean running;
+    private int previousSteps;
+    private int totalSteps;
+    private Context context;
+    private SensorManager sensorManager;
+    private Sensor stepCounterSensor;
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+    public void getSensorManager() {
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    //todo
+    public void useStepSensor() {
+        stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (stepCounterSensor == null){
+            stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);}
+
+        if (stepCounterSensor == null) {
+            Toast.makeText(context,
+                    "Required step counter sensor is not present on this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void prepare() {
+
+    }
+
+    public int countSteps() {
+        return 0;
+    }
+
+    public void resumeCounter() {
+        running = true;
+    }
+}
